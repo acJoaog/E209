5710f861e14cdaa9c606e35d4c9061918f5c1fc1
joao <joaog.azc@gmail.com>
2025-03-10 21:20:06 -0300

    relatorio3 fix

--- Lab/Relatorio3/exe3.c
+++ Lab/Relatorio3/exe3.c
@@ -1,35 +1,33 @@
 #define E0 0
 #define E1 1
 #define E2 2
-#define E3 3
 
 int main(){
 
   DDRD = 0b00000011;
-  PORTD = 0b00000001;
-  int estado = E0;
+  PORTD = 0b00000000;
+  int estado = E2;
 
   for(;;){
     if(PIND & 0b00000100){
         switch(estado){
             case E0:
-            PORTD = 0b00000001;
-            estado = E1;
+                PORTD = 0b00000001;
+                estado = E1;
             break;
 
             case E1:
-            PORTD = 0b00000010;
-            estado = E2;
+                PORTD = 0b00000010;
+                estado = E2;
             break;
 
             case E2:
-            PORTD = 0b00000000;
-            
-            estado = ;
+                PORTD = 0b00000000;
+                estado = E0;
             break;
 
             default:
-            PORTD = 0b00000000;
+                PORTD = 0b00000000;
             break;
         }
         _delay_ms(1000);
